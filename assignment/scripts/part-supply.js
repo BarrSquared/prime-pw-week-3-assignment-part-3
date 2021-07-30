1d0e14bdc216b0df921b207b4b47f3ed3a70ec9a
Russell Barr <rbarrsquared@gmail.com>
2021-07-30 10:18:21 -0500

    part 3, q 6-9

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -28,6 +28,20 @@ console.log('5. Adding 25 to supplyChanges.');
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.'
 console.log('6. Showing supplyChanges...');
+for (i = 0; i < supplyChanges.length; i++) {
+  if(supplyChanges[i] > 0){
+    console.log('Added ' + supplyChanges[i] + ' parts.');
+  } else if(supplyChanges[i] === 0){
+    console.log('No change.');
+  } else if(supplyChanges[i] < 0){
+    console.log('Removed ' + supplyChanges[i] + ' parts.');
+  }
+}
+
+// STRETCH GOALS
+console.log('---  Stretch Goals  ---');
+// 7. Rewrite the `for` loop from #6 as a `for of` loop.
+console.log('7. Showing supplyChanges with "for of" loop');
 for(x of supplyChanges){
   if(x > 0){
     console.log('Added ' + x + ' parts.');
@@ -38,15 +52,26 @@ for(x of supplyChanges){
   }
 }
 
-// STRETCH GOALS
-console.log('---  Stretch Goals  ---');
-// 7. Rewrite the `for` loop from #6 as a `for of` loop.
-console.log('7. Showing supplyChanges with "for of" loop');
-
 // 8. Rewrite the `for` loop from #6 as a `while` loop.
 console.log('8. Showing supplyChanges with "while" loop');
-
+let index = 0;
+while (index < supplyChanges.length){
+  if(supplyChanges[index] > 0){
+    console.log('Added ' + supplyChanges[index] + ' parts.');
+  } else if(supplyChanges[index] === 0){
+    console.log('No change.');
+  } else if(supplyChanges[index] < 0){
+    console.log('Removed ' + supplyChanges[index] + ' parts.');
+  }
+  index++;
+}
 
 // 9. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
-console.log('9. Total supplies available is:');
+let totalPartsAv = 0;
+for (j = 0; j < supplyChanges.length; j++) {
+  if(supplyChanges[j] > 0){
+    totalPartsAv = totalPartsAv + supplyChanges[j];
+  }
+}
+console.log('9. Total supplies available is:' + totalPartsAv);
